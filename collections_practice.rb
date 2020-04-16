2c701630d7ef00b681b260f89e4c42a12f389d99
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 00:50:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+def sort_array_asc(numbers)
+  numbers.sort do |a, b|
+  a <=> b
+  end
+end 
 
+def sort_array_desc(numbers)
+  numbers.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(string)
+  string.sort do |a, b|
+    
+  end
+end
